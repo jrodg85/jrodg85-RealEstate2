cbe98beec4e9c1c5bff8ae2549523988df039614
jrodg85 <lordbuba85@gmail.com>
2022-06-09 00:22:22 +0200

    321

--- app/src/main/java/es/jrodg85/realestate/Oferta.java
+++ app/src/main/java/es/jrodg85/realestate/Oferta.java
@@ -42,7 +42,7 @@ public abstract class Oferta {
 	}
 
 	public void setSuperficieVivienda() {
-		this.superficieVivienda = superficieVivienda();
+		this.superficieVivienda = getVivienda().getSuperficie();
 	}
 
 	// contructores
@@ -52,7 +52,7 @@ public abstract class Oferta {
 
 	public Oferta(String tituloOferta) {
 		this.tituloOferta = tituloOferta;
-		this.superficieVivienda = superficieVivienda();
+		this.superficieVivienda = getVivienda().getSuperficie();
 	}
 
 	public double superficieVivienda() {
